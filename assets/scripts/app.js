951139f7338301e44a989c40e63e829a4ff255dd
rainswerld <m.rainrobinson@gmail.com>
2020-07-16 14:57:26 -0600

    Added the necessary event listener
    Added the event listener for the 'Create Game' button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
+const gameActions = require('./game-actions/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
@@ -12,4 +13,7 @@ $(() => {
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-pw').on('submit', authEvents.onChangePw)
   $('#sign-out').on('submit', authEvents.onSignOut)
+
+  // Game listeners
+  $('#create-game').on('submit', gameActions.onCreateGame)
 })
