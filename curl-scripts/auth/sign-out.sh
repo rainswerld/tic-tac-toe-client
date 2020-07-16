817fcf75f238bbf56d931cf5a38060c05790e474
rainswerld <m.rainrobinson@gmail.com>
2020-07-16 13:55:15 -0600

    Added sign-out curl script
    New curl script for sign out added to curl-scripts/auth directory

--- curl-scripts/auth/sign-out.sh
+++ curl-scripts/auth/sign-out.sh
@@ -1,8 +1,5 @@
-curl --include --request DELETE "https://tic-tac-toe-api-production.herokuapp.com/sign-out" \
+curl --include --request DELETE "https://tic-tac-toe-api-development.herokuapp.com/sign-out" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
-  --data '{
-
-  }'
 
 echo
