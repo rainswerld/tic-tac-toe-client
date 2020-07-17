db9f29e704824c0b54a4f2a1e24bdca9f64a7298
rainswerld <m.rainrobinson@gmail.com>
2020-07-17 09:44:28 -0600

    Success & Failure messages coded
    Have the success and failure messages for the AJAX request

--- assets/scripts/game-actions/ui.js
+++ assets/scripts/game-actions/ui.js
@@ -4,20 +4,32 @@ const store = require('../store')
 
 const createGameSuccess = function (response) {
   $('#message').text('You created a game')
-
+  store.game = response.game
   const oneGame = (`
     <h3>${response.game.cells}</h3>
     <h4>${response.game.over}</h4>
-    <h4>${response.game.owner}</h4>
+    <h4>${response.game._id}</h4>
     `)
   $('#content').html(oneGame)
 }
 
-const createGameFailed = function () {
+const createGameFailed = function (response) {
   $('#message').text('You failed to create a game')
+  store.game = response.game
+  console.log(store.game)
+}
+
+const boardUpdateSuccess = function (response) {
+  console.log(response)
+}
+
+const boardUpdateFailed = function (error) {
+  console.log(error)
 }
 
 module.exports = {
   createGameSuccess,
-  createGameFailed
+  createGameFailed,
+  boardUpdateSuccess,
+  boardUpdateFailed
 }
