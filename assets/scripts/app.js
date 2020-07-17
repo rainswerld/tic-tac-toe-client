dac7a71e27f6a99fe69d4109333c01b1c30cfcbc
rainswerld <m.rainrobinson@gmail.com>
2020-07-17 09:42:15 -0600

    Added event listener to first box
    Successfully added an event listener to first box

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,5 +16,5 @@ $(() => {
 
   // Game listeners
   $('#create-game').on('submit', gameActions.onCreateGame)
-  $('#test').on('submit', gameActions.onBoardUpdate)
+  $('#box-one').on('click', gameActions.onBoardUpdate)
 })
